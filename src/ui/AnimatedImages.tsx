aea8cd6101e007d70c25daac5ccede25916d5763
ktcotz <naskret.kamil@gmail.com>
2024-07-22 18:23:02 +0200

    feat: animated images
    Work around animated images.

--- src/ui/AnimatedImages.tsx
+++ src/ui/AnimatedImages.tsx
@@ -1,3 +1,43 @@
+type ImageProps = {
+  path: string;
+  delay: string;
+  alt: string;
+};
+
+const Image = ({ path, delay, alt }: ImageProps) => {
+  return (
+    <img
+      src={path}
+      alt={alt}
+      className="animate-fade-image absolute top-[26px] right-[57px]"
+      style={{
+        animationDelay: delay,
+      }}
+      width={250}
+      height={541}
+    />
+  );
+};
+
 export const AnimatedImages = () => {
-  return <div>ASD</div>;
+  return (
+    <div
+      className="w-[465px] h-[635px] bg-no-repeat bg-cover place-self-end relative"
+      style={{
+        backgroundImage: `url("./images/home-phones.png")`,
+      }}
+    >
+      <Image
+        path="./images/screenshot1.png"
+        delay="0"
+        alt="Home screen of InstaLove"
+      />
+      <Image path="./images/screenshot2.png" delay="3s" alt="Chat" />
+      <Image
+        path="./images/screenshot3.png"
+        delay="6s"
+        alt="Individual profile on InstaLove"
+      />
+    </div>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ export default {
       fontFamily: {
         sans: ["Roboto", "sans-serif"],
       },
+      animation: {
+        "fade-image": "fadeInOut 9s infinite;",
+      },
+      keyframes: {
+        fadeInOut: {
+          "0%": { opacity: 0 },
+          "10%": { opacity: 1 },
+          "30%": { opacity: 1 },
+          "40%": { opacity: 0 },
+          "100%": { opacity: 0 },
+        },
+      },
     },
   },
   plugins: [],
